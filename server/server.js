a92713a36bea4c34f1cb964a7d666d0f08097013
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-06-10 16:10:58 +0200

    setting up sendGrid

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const sgMail = require('@sendgrid/mail');
 
 // client connection
 const cors = require('cors')
@@ -12,10 +13,35 @@ const PORT = process.env.PORT || 8000;
 app.use(cors());
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.json('hello')
+})
+
+app.post('/contact/email', (req, res) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const { name, email, message, title } = req.body
+    const msg = {
+        to: 'a.alghetheth@gmail.com',
+        from: 'a.alghetheth@gmail.com',
+        subject: req.body.title,
+        templateId: process.env.templateId,
+        dynamicTemplateData: {
+            name,
+            email,
+            message,
+            title
+        }
+
+    }
+    sgMail
+        .send(msg)
+        .then(() => {
+            console.log('Email Successfully sent!')
+            res.json('Email Successfully sent.')
+        })
+        .catch(err => console.log(err))
 })
 
 
